ee1ce43fc759dfa478162307fd352dfb0427fc14
Matthieu Champigny <matthieu.champigny@free.fr>
2026-03-05 12:01:32 +0100

    One step

---
+++ Local_browser.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+# Initialisation de la page 
+
+fenetre = Tk()
+fenetre.title("ESEOFOX")
+fenetre.iconbitmap("IconBitMap_ESEOFOX.ico")
+fenetre.config(background= "#c4c4c4")
+
+
+
+
+fenetre.mainloop()
