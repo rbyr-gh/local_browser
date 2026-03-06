eef9da8e073255236c76aab4c1cb9cbb69197f7f
matthieuags-cyber <matthieu.ags@gmail.com>
2026-03-06 10:49:17 +0100

    morpion

--- morpion.py
+++ morpion.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 
 fenetre = Tk()
 ws = fenetre.winfo_screenwidth()
@@ -8,8 +9,6 @@ y = (hs/2) - (600/2)
 fenetre.geometry('%dx%d+%d+%d' % (600,640,x,y))
 fenetre.focus_force()
 fenetre.title("Morpion")
-img = Image("photo", file="morpion.png")
-fenetre.iconphoto(True, img) 
 
 tab1 = [[0,0,0],[0,0,0],[0,0,0]]
 tour = 1
@@ -17,7 +16,7 @@ tour = 1
 joueur1 = Label(fenetre, text = "Joueur 1 : ")
 joueur1.pack(side=TOP)
 
-joueur2 = Label(fenetre, text = "Joueur 2 : ")
+joueur2 = Label(fenetre, text = "Ordinateur : ")
 joueur2.pack(side=TOP)
 
 cnv = Canvas(fenetre,width=600,height=600,bg="white")
@@ -61,10 +60,11 @@ def fenetreVictoire(tab,forme) :
     btn_quitter2.pack(side=RIGHT)
     
     vider()
-     
+
 def vider() :
-    global tab1
+    global tab1,tour
     tab1 = [[0,0,0],[0,0,0],[0,0,0]]
+    tour = 1
     
     cnv.delete('all')
     
@@ -74,9 +74,62 @@ def vider() :
     cnv.create_line(75,225,525,225,fill="black")
     cnv.create_line(75,375,525,375,fill="black")
 
+def dessinerX(i,j):
+    if i == 0 and j == 0:
+        cnv.create_line(90,90,210,210,fill="red")
+        cnv.create_line(210,90,90,210,fill="red")
+    elif i == 0 and j == 1:
+        cnv.create_line(90,240,210,360,fill="red")
+        cnv.create_line(210,240,90,360,fill="red")
+    elif i == 0 and j == 2:
+        cnv.create_line(90,390,210,510,fill="red")
+        cnv.create_line(210,390,90,510,fill="red")
+
+    elif i == 1 and j == 0:
+        cnv.create_line(240,90,360,210,fill="red")
+        cnv.create_line(360,90,240,210,fill="red")
+    elif i == 1 and j == 1:
+        cnv.create_line(240,240,360,360,fill="red")
+        cnv.create_line(360,240,240,360,fill="red")
+    elif i == 1 and j == 2:
+        cnv.create_line(240,390,360,510,fill="red")
+        cnv.create_line(360,390,240,510,fill="red")
+
+    elif i == 2 and j == 0:
+        cnv.create_line(390,90,510,210,fill="red")
+        cnv.create_line(510,90,390,210,fill="red")
+    elif i == 2 and j == 1:
+        cnv.create_line(390,240,510,360,fill="red")
+        cnv.create_line(510,240,390,360,fill="red")
+    elif i == 2 and j == 2:
+        cnv.create_line(390,390,510,510,fill="red")
+        cnv.create_line(510,390,390,510,fill="red")
+
+def ordinateur():
+    global tour
+
+    cases_libres = []
+
+    for i in range(3):
+        for j in range(3):
+            if tab1[i][j] == 0:
+                cases_libres.append((i,j))
+
+    if len(cases_libres) == 0:
+        return
+
+    i,j = random.choice(cases_libres)
+
+    tab1[i][j] = 2
+    dessinerX(i,j)
+
+    tour = 1
+    verifVictoire(tab1,2)
+
 def clic(event) :
     global tour
     x,y = event.x , event.y
+
     if tour == 1 :
         if 75 < x < 225 :
             if 75 < y < 225 and tab1[0][0] == 0 :
@@ -117,59 +170,12 @@ def clic(event) :
                 tab1[2][2] = 1
                 tour = 2 
                 cnv.create_oval(390,390,510,510,outline="blue")
+
         verifVictoire(tab1,1)
-    elif tour == 2 :
-        if 75 < x < 225 :
-            if 75 < y < 225 and tab1[0][0] == 0 :
-                tab1[0][0] = 2
-                cnv.create_line(90,90,210,210,fill="red")
-                cnv.create_line(210,90,90,210,fill="red")
-                tour = 1
-            elif 225 < y < 375 and tab1[0][1] == 0 :
-                tab1[0][1] = 2
-                cnv.create_line(90,240,210,360,fill="red")
-                cnv.create_line(210,240,90,360,fill="red")
-                tour = 1 
-            elif 375< y < 525 and tab1[0][2] == 0 :
-                tab1[0][2] = 2
-                cnv.create_line(90,390,210,510,fill="red")
-                cnv.create_line(210,390,90,510,fill="red")
-                tour = 1 
-        elif 225 < x < 375 :
-            if 75 < y < 225 and tab1[1][0] == 0:
-                tab1[1][0] = 2
-                cnv.create_line(240,90,360,210,fill="red")
-                cnv.create_line(360,90,240,210,fill="red")
-                tour = 1 
-            elif 225 < y < 375 and tab1[1][1] == 0 :
-                tab1[1][1] = 2
-                cnv.create_line(240,240,360,360,fill="red")
-                cnv.create_line(360,240,240,360,fill="red")
-                tour = 1 
-            elif 375< y < 525 and tab1[1][2] == 0 :
-                tab1[1][2] = 2
-                cnv.create_line(240,390,360,510,fill="red")
-                cnv.create_line(360,390,240,510,fill="red")
-                tour = 1 
-        elif 375 < x < 525 :
-            if 75 < y < 225 and tab1[2][0] == 0 :
-                tab1[2][0] = 2
-                cnv.create_line(390,90,510,210,fill="red")
-                cnv.create_line(510,90,390,210,fill="red")
-                tour = 1 
-            elif 225 < y < 375 and tab1[2][1] == 0 :
-                tab1[2][1] = 2
-                cnv.create_line(390,240,510,360,fill="red")
-                cnv.create_line(510,240,390,360,fill="red")
-                tour = 1 
-            elif 375< y < 525 and tab1[2][2] == 0 :
-                tab1[2][2] = 2
-                cnv.create_line(390,390,510,510,fill="red")
-                cnv.create_line(510,390,390,510,fill="red")
-                tour = 1 
-        verifVictoire(tab1,2)
-    
-    
+
+        if tour == 2:
+            fenetre.after(300, ordinateur)
+
 cnv.bind("<Button-1>",clic)
 
 btn_quitter = Button(fenetre,text="Quitter",command=fenetre.destroy)
@@ -177,6 +183,5 @@ btn_quitter.pack(side=RIGHT)
 
 btn_rejouer = Button(fenetre,text="Rejouer", command=vider)
 btn_rejouer.pack(side=LEFT)
-
 
 fenetre.mainloop()
