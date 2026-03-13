fb97a7e523644e81ee554269d0e16d3d5a2ef158
Matthieu Champigny <matthieu.champigny@free.fr>
2026-03-13 09:08:27 +0100

    step 5

--- Local_browser.py
+++ Local_browser.py
@@ -13,8 +13,8 @@ class App(ctk.CTk):
         super().__init__()
 
         # Configuration de la fenêtre principale
-        self.title("ESEOFOX")
-        self.geometry("900x600")
+        self.title("ESEOFOX") 
+        self.attributes('-fullscreen', True)
         
         # Configuration de la grille (2 colonnes : Menu + Contenu)
         self.grid_columnconfigure(1, weight=1)
@@ -23,22 +23,25 @@ class App(ctk.CTk):
         # --- 1. CRÉATION DU MENU LATÉRAL (Gauche) ---
         self.sidebar_frame = ctk.CTkFrame(self, width=200, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, sticky="nsew")
-        self.sidebar_frame.grid_rowconfigure(6, weight=1) # Espace flexible en bas
+        self.sidebar_frame.grid_rowconfigure(7, weight=1) # Espace flexible en bas
 
         # Titre du menu
         self.logo_label = ctk.CTkLabel(self.sidebar_frame, text="ESEOFOX", font=ctk.CTkFont(size=20, weight="bold"))
-        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(100, 10))
 
         # Boutons du menu
-            # Boutons d'accueil
+            # Bouton d'accueil
         self.btn_home = ctk.CTkButton(self.sidebar_frame, text="Accueil", command=self.show_home)
         self.btn_home.grid(row=1, column=0, padx=20, pady=10)
-            # Boutons des paramètres
+            # Bouton des paramètres
         self.btn_settings = ctk.CTkButton(self.sidebar_frame, text="Paramètres", command=self.show_settings)
         self.btn_settings.grid(row=2, column=0, padx=20, pady=10)
-            # Boutons des aides
+            # Bouton des aides
         self.btn_help = ctk.CTkButton(self.sidebar_frame, text="Aide", command=self.show_help)
         self.btn_help.grid(row=3, column=0, padx=20, pady=10)
+            # Bouton pour quitter la page
+        self.btn_quit = ctk.CTkButton(self.sidebar_frame, text="Quitter la page", command = self.quit)
+        self.btn_quit.grid(row=7, column=0, padx=20, pady=(0,50), sticky='s')
 
         # Interrupteur de thème (dans le menu)
         self.appearance_mode_label = ctk.CTkLabel(self.sidebar_frame, text="Mode :", anchor="w")
@@ -67,33 +70,37 @@ class App(ctk.CTk):
         self.app_label.grid(row=1, column=0, padx=10, pady=10, sticky="w")
 
         # Logo App
-        chronoImage = ctk.CTkImage(light_image=Image.open("Chrono_lightmode.png"), dark_image=Image.open("Chrono_darkmode.png"), size=(90, 90))
-        button_chronoImage = ctk.CTkButton(self.content_frame_accueil, image=chronoImage, text="", fg_color= "transparent")
-        button_chronoImage.grid(row=2, column=0, padx=40, pady=10)
+        chronoImage = ctk.CTkImage(light_image=Image.open("Chrono_lightmode.png"), dark_image=Image.open("Chrono_darkmode.png"), size=(150, 150))
+        button_chronoImage = ctk.CTkButton(self.content_frame_accueil, image=chronoImage, text="", fg_color= "transparent", command=self.run_chrono)
+        button_chronoImage.grid(row=2, column=0, padx=100, pady=10, sticky="w")
 
-        snakeImage = ctk.CTkImage(light_image=Image.open("Snake.png"), size=(90, 90))
-        button_snakeImage = ctk.CTkButton(self.content_frame_accueil, image=snakeImage, text="", fg_color= "transparent")
-        button_snakeImage.grid(row=2, column=1, padx=40, pady=10)
+        snakeImage = ctk.CTkImage(light_image=Image.open("Snake.png"), size=(150, 150))
+        button_snakeImage = ctk.CTkButton(self.content_frame_accueil, image=snakeImage, text="", fg_color= "transparent", command=self.run_snake)
+        button_snakeImage.grid(row=2, column=1, padx=100, pady=10, sticky="nsew")
 
-        notesImage = ctk.CTkImage(light_image=Image.open("Notes.png"), size=(90, 90))
-        button_notesImage = ctk.CTkButton(self.content_frame_accueil, image=notesImage, text="", fg_color= "transparent")
-        button_notesImage.grid(row=2, column=2, padx=40, pady=10)
+        notesImage = ctk.CTkImage(light_image=Image.open("Notes.png"), size=(150, 150))
+        button_notesImage = ctk.CTkButton(self.content_frame_accueil, image=notesImage, text="", fg_color= "transparent", command=self.run_notes)
+        button_notesImage.grid(row=2, column=2, padx=100, pady=10, sticky="e")
 
-        morpionImage = ctk.CTkImage(light_image=Image.open("Morpion.png"), size=(90, 90))
-        button_morpionImage = ctk.CTkButton(self.content_frame_accueil, image=morpionImage, text="", fg_color= "transparent")
-        button_morpionImage.grid(row=3, column=0, padx=40, pady=10)
+        morpionImage = ctk.CTkImage(light_image=Image.open("Morpion.png"), size=(150, 150))
+        button_morpionImage = ctk.CTkButton(self.content_frame_accueil, image=morpionImage, text="", fg_color= "transparent", command=self.run_morpion)
+        button_morpionImage.grid(row=3, column=0, padx=100, pady=10, sticky="w")
 
-        contactsImage = ctk.CTkImage(light_image=Image.open("Contacts.png"), size=(90, 90))
-        button_contactsImage = ctk.CTkButton(self.content_frame_accueil, image=contactsImage, text="", fg_color= "transparent")
-        button_contactsImage.grid(row=3, column=1, padx=40, pady=10)
+        contactsImage = ctk.CTkImage(light_image=Image.open("Contacts.png"), size=(150, 150))
+        button_contactsImage = ctk.CTkButton(self.content_frame_accueil, image=contactsImage, text="", fg_color= "transparent", command=self.run_contacts,height=170)
+        button_contactsImage.grid(row=3, column=1, padx=100, pady=0, sticky="we")
 
-        messagesImage = ctk.CTkImage(light_image=Image.open("Messages.png"), size=(90, 90))
-        button_messagesImage = ctk.CTkButton(self.content_frame_accueil, image=messagesImage, text="", fg_color= "transparent")
-        button_messagesImage.grid(row=3, column=2, padx=40, pady=10)
+        messagesImage = ctk.CTkImage(light_image=Image.open("Messages.png"), size=(150, 150))
+        button_messagesImage = ctk.CTkButton(self.content_frame_accueil, image=messagesImage, text="", fg_color= "transparent", command=self.run_messages)
+        button_messagesImage.grid(row=3, column=2, padx=100, pady=0, sticky="e")
 
-        wikiImage = ctk.CTkImage(light_image=Image.open("Wiki.png"), size=(90, 90))
-        button_wikiImage = ctk.CTkButton(self.content_frame_accueil, image=wikiImage, text="", fg_color= "transparent")
-        button_wikiImage.grid(row=4, column=0, padx=40, pady=10)
+        wikiImage = ctk.CTkImage(light_image=Image.open("Wiki.png"), size=(150, 150))
+        button_wikiImage = ctk.CTkButton(self.content_frame_accueil, image=wikiImage, text="", fg_color= "transparent", command=self.run_wiki)
+        button_wikiImage.grid(row=4, column=0, padx=100, pady=0, sticky="w")
+
+        calculatriceImage = ctk.CTkImage(light_image=Image.open("Calculatrice.png"), size=(150, 150))
+        button_calculatriceImage = ctk.CTkButton(self.content_frame_accueil, image=calculatriceImage, text="", fg_color= "transparent", command=self.run_calculatrice)
+        button_calculatriceImage.grid(row=4, column=1, padx=100, pady=10, sticky="nsew")
 
         # FRAME PARAMETRE
         self.content_frame_settings = ctk.CTkFrame(self, corner_radius=0, fg_color="transparent")
@@ -133,6 +140,30 @@ class App(ctk.CTk):
         self.content_frame_accueil.grid_forget()
         self.content_frame_settings.grid_forget()
         self.content_frame_help.grid(row=0, column=1, sticky="nsew", padx=20, pady=20)
+
+    def run_snake(self):
+        pass
+
+    def run_morpion(self):
+        pass
+
+    def run_messages(self):
+        pass
+
+    def run_chrono(self):
+        pass
+
+    def run_notes(self):
+        pass
+
+    def run_contacts(self):
+        pass
+
+    def run_wiki(self):
+        pass
+
+    def run_calculatrice(self):
+        pass
 
 if __name__ == "__main__":
     app = App()
